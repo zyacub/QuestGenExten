9112276dc1c5a6067bc5b62901bc82970117b74d
zyacub <113848867+zyacub@users.noreply.github.com>
2023-10-02 20:37:42 -0500

    Delete QuestGen/backend directory
    Replacing Backend (deleting old)

--- QuestGen/backend/config.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-DATABASE_URL = "postgresql://postgres:root@localhost:5432/questiondb"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
